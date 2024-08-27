a39f4176f3d7fd63faef65ab5e0cef014fea027e
abhishekode <iabhishekn98@gmail.com>
2024-08-27 15:40:48 +0530

    add column for

--- src/middleware/request-logger.middleware.ts
+++ src/middleware/request-logger.middleware.ts
@@ -4,16 +4,16 @@ import type { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
-	private readonly logger = new Logger();
+  private readonly logger = new Logger();
 
-	use(req: Request, res: Response, next: NextFunction) {
-		res.on('finish', () => {
-			const statusCode = res.statusCode;
-			if (statusCode === 401 || statusCode === 404 || statusCode === 405) {
-				this.logger.warn(`[${req.method}] ${req.url} - ${statusCode}`);
-			}
-		});
+  use(req: Request, res: Response, next: NextFunction) {
+    res.on('finish', () => {
+      const statusCode = res.statusCode;
+      if (statusCode === 401 || statusCode === 404 || statusCode === 405) {
+        this.logger.warn(`[${req.method}] ${req.url} - ${statusCode}`);
+      }
+    });
 
-		next();
-	}
+    next();
+  }
 }
